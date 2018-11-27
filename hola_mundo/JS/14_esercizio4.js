f7142e2ac2e562fcc2eb0b03a41840d0f52fc26f
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2018-11-27 17:29:44 +0100

    File updated

--- hola_mundo/JS/14_esercizio4.js
+++ hola_mundo/JS/14_esercizio4.js
@@ -3,13 +3,11 @@
 // Mostrami in console tutti i numeri dispari tra i due numeri introdotti dallo user
 
 var num1 = parseInt(prompt("Introduci il primo numero", 0));
-var num2 = parseInt(prompt("Introduci il secondo numero, 0));
-
-//ricorriamo tra i due numeri introdotti dallo user
+var num2 = parseInt(prompt("Introduci il secondo numero", 0));
 
 while(num1 < num2){
-    if(num1%2 !=0)
-{
-    console.log("Il " +num1+" è dispari");
-} num1++;
+    num1++;
+    if(numero1%2 !=0){
+        console.log("Il " +num1+" è dispari");
+    }
 }
