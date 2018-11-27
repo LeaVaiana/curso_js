cb3e8b472a8b4859de679f93ed27a161cac62d53
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2018-11-27 18:35:55 +0100

    File edited

--- hola_mundo/JS/17_esercizio7.js
+++ hola_mundo/JS/17_esercizio7.js
@@ -1,24 +1,19 @@
 'use strict'
 
-//table de multiplicar de un numero introducido por pantalla (del 1 al 10)
-//creiamo il promtp del numero 
+//Crea la tavola di moltiplicazione di un numero introdotto dallo user
 
-var numero = parseInt(prompt("De que numero quieres la tabla",1))
+var num = parseInt(prompt("Di quale numero vuoi la tavola?",1))
 
-document.write("<h1>Tabla del "+numero+" </h1>");
+document.write("<h1>Tavola del "+num+" </h1>");
 for(var i = 1; i <= 10; i++){
-    document.write(i+" x "+numero+" = " + (i*numero)+"<br/>");
+    document.write(i+" x "+num+" = " + (i*num)+"<br/>");
 }
 
-//esempio numero = 2 alla seconda iterazione, pvvero per i = 2 avremo
-                    //(2+" x "2" = " + (risultado di 2*2)
 
-                    // per fare la formazzatione aggiungo il +"<br/>"
-
-//todas las tablas de multiplicar, faccio un for per tutti i numeri e il codice sará quello di sopra
+//Per tutte le tavole, racchiudo il codice dentro un for, con la variabile c da 1 a 10. 
 
 for(var c = 1; c <=10; c++){
-    document.write("<h1>Tabla del "+c+" </h1>");
+    document.write("<h1>Tavola del "+c+" </h1>");
     for(var i = 1; i <= 10; i++){
     document.write(i+" x "+c+" = " + (i*c)+"<br/>");
 }
