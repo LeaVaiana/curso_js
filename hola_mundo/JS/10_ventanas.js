67ece6fc715506783b04937d07016006e327b304
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2018-11-27 12:48:15 +0100

    File updated

--- hola_mundo/JS/10_ventanas.js
+++ hola_mundo/JS/10_ventanas.js
@@ -9,10 +9,10 @@ confirm("Seguro que quieres continuar?");
 
 //posso mettere il confirm en una variable
 var mi_resultado = confirm ("Estas seguro que quieres continuar?");
-console.log(mi_resultado);//a questo punto il mio risultato in console sará true
+console.log(mi_resultado);//true
 
 //ventana de ingreso de datos, con dos parametros
-prompt("Qeue edad tienes",18);//se lo metto dentro una variabile ottengo il resultado:
+prompt("Que edad tienes",18);//se lo metto dentro una variabile ottengo il resultado:
 
-//var mi-resultado = prompt("Que edad tienes", 18);
-//console.log(mi_resultado);
+var mi_resultado = prompt("Que edad tienes", 18);
+console.log(mi_resultado);
