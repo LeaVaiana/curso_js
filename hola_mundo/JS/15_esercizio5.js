0545980d82d43eb353e977cdac8f889f6cefa2ab
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2018-11-27 18:03:36 +0100

    File edited

--- hola_mundo/JS/15_esercizio5.js
+++ hola_mundo/JS/15_esercizio5.js
@@ -1,13 +1,11 @@
 'use strict'
+//Mostrami in consola tutti i numeri divisori del numero introdotto
 
-//programa que muestre todos los numero divisores de un numero introducidos en un prompt
-//creiamo la variabile donde se guarda el numero introducido por el usuario
-//un divisore è quando un numero se divide por el divisor como resto nos da zero, numero/ i (il numero che stiamo ricorrendo in quel momento)
-var numero = prompt("mete un numero", 1);
+var num = parseInt(prompt("Introduci un numeri",1));
 
-for(var i = 1; i <= numero; i ++){
-    if(numero%i ==0){
-        console.log("Divisor: " + i);
+for(var i = 1; i <= num; i ++){
+    if(num%i ==0){
+        console.log("Divisore: " + i);
     }
     
 }
