1764f02a0323e29271e9f27e4f4b51f9e54ecc92
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2018-11-27 16:18:42 +0100

    File updated

--- hola_mundo/JS/11_esercizio1.js
+++ hola_mundo/JS/11_esercizio1.js
@@ -1,21 +1,22 @@
 'use strict'
 
-//programa que pida dos numeros y nos diga cual es mayor, cual menor y si son iguales
-//plus: si los numeros no son un numero o son menores o iguales a zero que nos lo vuelva a pedir
-//el parseint para convertir el string que coge el numero en numero, numero per difetto è zero'
-var numero1 = parseInt(prompt("Introduce el primer numero", 0)); 
-var numero2 = parseInt(prompt("Introduce el segundo numero", 0));
+//Introduci 2 numeri e determina quale è maggiore, minore o se sono uguali. 
+//Se i numeri inseriti sono <=0, vogliamo che il programma richieda di inserirli
+//(parseInt converte il valore stringa inserito nel prompt in numero)
+
+var numero1 = parseInt(prompt("Introduci il primo numero", 0)); 
+var numero2 = parseInt(prompt("Introduci il secondo numero", 0));visu
 
 //console.log(numero1, numero2); si no hubiera escrito el plus
 while(numero1 <=0 || numero2 <=0|| isNaN(numero1) || isNaN(numero2)){
-    numero1 = parseInt(prompt("introduce el primer numero", 0));
-    numero2 = parseInt(prompt("introduce el segundo numero", 0));
+    numero1 = parseInt(prompt("Introduci il primo numero", 0));
+    numero2 = parseInt(prompt("Introduci il secondo numero", 0));
 }
 if(numero1 == numero2){
-    alert("Los numeros son iguales");
+    alert("I numeri sono uguali");
 }else if(numero1 > numero2){
-    alert("el numero mayor es " + numero1);
-    alert("el numero menor es " + numero2);
+    alert("Il numero maggiore è " + numero1);
+    alert("Il numero minore è " + numero2);
 }else{
-    alert("el numero mayor es " + numero2);
+    alert("Il numero maggiore è " + numero2);
 }
