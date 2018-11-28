98b87afee214ac09204e31dfc26b475c574c7355
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2018-11-28 10:36:46 +0100

    File updated
    Calculator.
    Results in the body and as alert

--- hola_mundo/JS/18_esercizio8.js
+++ hola_mundo/JS/18_esercizio8.js
@@ -1,31 +1,27 @@
 'use strict'
 
-// Calculadora
-//pida dos numeros por pantalla
-//si metemos uno mal (negativo o NaN) que nos lo vuelva a pedir (lo que hace el while)
-// en el body, alert y console il resultado de sumar, restar, multiplicar y dividir
-//el resultado es una stringa che con document.write lo trovo nel body
+// Calcolatrice: inseriti due numeri, calcolarne la somma, differenza, moltiplicazione e divisione.
+// Mostrare i risultati nel body del documento HTML (document.write) e in console e come alert.
 
-var numero1 = parseInt(prompt("introduce un numero",0));
-var numero2 = parseInt(prompt("introduce un numero",0));
+var num1 = parseInt(prompt("Introduci un numero",0));
+var num2 = parseInt(prompt("Introduci un numero",0));
 
-while(numero1 < 0 || numero2 <0 || isNaN(numero1)|| isNaN(numero2)){
-    numero1 = parseInt(prompt("introduce un numero",0));
-    numero2 = parseInt(prompt("introduce un numero",0));
+while(num1 < 0 || num2 <0 || isNaN(num1)|| isNaN(num2)){
+    num1 = parseInt(prompt("Introduci un numero",0));
+    num2 = parseInt(prompt("Introduci un numero",0));
 }
 
-var resultado = "la suma es: "+(numero1+numero2)+" <br/>"+
-                "la resta es: "+(numero1-numero2)+" <br/>"+
-                "la multiplicacion es: "+(numero1*numero2)+" <br/>"+
-                "la division es: "+(numero1/numero2)+" <br/>";
+var risultato = "la somma è: "+(num1+num2)+" <br/>"+
+                "la sottrazione è: "+(num1-num2)+" <br/>"+
+                "la moltiplicazione è: "+(num1*num2)+" <br/>"+
+                "la divisione è: "+(num1/num2)+" <br/>";
 
-document.write(resultado)
-//devo fare due var resultado, porque con document.write il salto di linea lo faccio con br, in console il salto di linea lo faccio con \n
-var resultadoCMD = "la suma es: "+(numero1+numero2)+" \n"+
-                "la resta es: "+(numero1-numero2)+" \n"+
-                "la multiplicacion es: "+(numero1*numero2)+" \n"+
-                "la division es: "+(numero1/numero2)+" \n";
 
-document.write(resultado);
-alert(resultadoCMD);
-console.log(resultadoCMD);
+var risultatoCMD =  "la somma è: "+(num1+num2)+" \n"+
+                    "la sottrazione è: "+(num1-num2)+" \n"+
+                    "la moltiplicazione è: "+(num1*num2)+" \n"+
+                    "la divisione è: "+(num1/num2)+" \n";
+
+document.write(risultato);
+alert(risultatoCMD);
+console.log(risultatoCMD);
