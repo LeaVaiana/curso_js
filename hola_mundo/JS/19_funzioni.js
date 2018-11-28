0ad24b3e3fcd8a501d646e18dc60d863d76cec82
Lea Vaiana <leavaiana@gmail.com>
2018-11-28 12:38:00 +0100

    Functio inside function

---
+++ hola_mundo/JS/19_funzioni.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Funzioni (gruppo di ordini/codice riutilizzabile)
+
+function nomefunzione(){
+    return "Buongiorno"; //la funzione mi ritorna un testo
+}
+
+//invoco la funzione:
+console.log(nomefunzione());
