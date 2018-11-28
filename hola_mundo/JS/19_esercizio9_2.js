28ffc6c8da52e6d3a1a0641352aa104fed93fdbe
Lea Vaiana <43003349+LeaVaiana@users.noreply.github.com>
2018-11-28 12:06:44 +0100

    File updated
    FUNZIONI JS

--- hola_mundo/JS/19_esercizio9_2.js
+++ hola_mundo/JS/19_esercizio9_2.js
@@ -1,53 +1,53 @@
 'use strict'
-/*
-//funciones y parametros
-//1) defino la funcion
-//2 defino las instrucciones
-//invo o llamo la funcion
+*
+//Funzioni contenenti parametri
+//1) Definisco la funzione
+//2) Definisco le istruzioni
+// Chiamo la funzione */
 
-function calculadora(numero1, numero2){
-    console.log("suma "+ (numero1+numero2));
-    console.log("resta "+ (numero1-numero2));
-    console.log("Multiplicacion "+ (numero1*numero2));
-    console.log("Division "+ (numero1/numero2));
+function calcolatrice(num1, num2){
+    console.log("Somma "+ (num1+num2));
+    console.log("Sottrazione "+ (num1-num2));
+    console.log("Moltiplicazione "+ (num1*num2));
+    console.log("Divisione "+ (num1/num2));
     console.log("************");
 
-    //return("ciao!!!")
-
+    return("ciao!!!")
 }
 
-//calculadora(3,3) // introduco due numeri a scelta
+//quando invoco la funzione, perchè esegua le istruzioni è necessario inserire due parametri, esempio (2,3)
+calcolatrice (2,3)
 
-//posso inserire i parametri dentro una for loop
+
+
+//Posso inserire i parametri (variabile i e numero scelto) dentro un for loop
 
 for(var i = 1; i<=10; i++){
-    console.log(i)//per vedere a che iterazione sono arrivata
-    calculadora(i,8);//parametro i e numero a scelta
+    console.log(i) //per vedere a che iterazione sono arrivata
+    calculadora(i,8);
 }
 
 
-//parametri opzionali
-//quando invoco la funzione dovrei inserirgli i parametri altrimenti mi darebbe errore
-//altrimenti la funzionalitá di parametri opzionali (esempio mostrar)*/
+//Posso scegliere se stampare i risultati in console o in html (3 parametro false o true)
 
-function calculadora(numero1, numero2, mostrar = false){
+function calcolatrice(num1, num2, mostrare = false){
     //conjunto de instrucciones a ejecturar
-    if(mostrar == false){
-        console.log("suma "+ (numero1+numero2));
-        console.log("resta "+ (numero1-numero2));
-        console.log("Multiplicacion "+ (numero1*numero2));
-        console.log("Division "+ (numero1/numero2));
+    if(mostrare == false){
+        console.log("somma "+ (num1+num2));
+        console.log("sottrazione "+ (num1-num2));
+        console.log("moltiplicazione "+ (num1*num2));
+        console.log("divisione "+ (num1/num2));
         console.log("************");
     }else{
-        document.write("suma "+ (numero1+numero2)+"</br>");
-        document.write("resta "+ (numero1-numero2)+"</br>");
-        document.write("Multiplicacion "+ (numero1*numero2)+"</br>");
-        document.write("Division "+ (numero1/numero2)+"</br>"CP);
+        document.write("somma "+ (num1+num2)+"</br>");
+        document.write("sottrazione "+ (num1-num2)+"</br>");
+        document.write("moltiplicazione "+ (num1*num2)+"</br>");
+        document.write("divisione "+ (num1/num2)+"</br>");
         document.write("************");
 
     }
 }
-//invoco in questo modo
-calculadora(1,4,false);//false è opzionale, perche mi stamperebbe in consola cmq la prima parte del codice
-calculadora(1,4,true);//se voglio che mi stampi in html devo mettere true, per questo ho aggiunto anche il br
+
+calcolatrice(1,4,false);//false per stampare in console
+calcolatrice(1,4,true);//se voglio che mi stampi anche nell'html
 
