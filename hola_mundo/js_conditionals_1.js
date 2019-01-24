0358bb0053b3a5911befb7da05bfae1edcf3570b
Lea Vaiana <leavaiana@gmail.com>
2019-01-24 16:33:33 +0100

    operadores

---
+++ hola_mundo/js_conditionals_1.js
@@ -0,0 +1,11 @@
+//js conditionals
+var age = Number(prompt("what's your age"));
+if(age < 18){
+console.log("you cannot enter")
+}
+else if(age < 21){
+  console.log("you can enter not drink")
+}
+else {
+  console.log("come in and drink")
+}
